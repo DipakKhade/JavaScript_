70abc61653fee8c4e3bb965177b7198d771337e7
= <=>
2023-09-09 18:27:01 +0530

    added upto 85

---
+++ tute84.js
@@ -0,0 +1,18 @@
+class Password{
+    constructor(no_upper_case,no_lower_case,no_specialchr){
+        this.no_lower_case=no_lower_case
+        this.no_upper_case=no_upper_case
+        this.no_specialchr=no_specialchr
+    }
+
+    strongPassword(){
+
+       
+
+
+    }
+    weakPassword(){
+
+    }
+
+}
